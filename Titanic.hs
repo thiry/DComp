1a74be1602fe01f2af1c0d99d42bd3280542c5ef
Laurent Thiry <laurent.thiry@uha.fr>
2017-05-26 12:51:41 +0200

    .

--- Titanic.hs
+++ Titanic.hs
@@ -1,6 +1,8 @@
 import Text.CSV -- cabal install csv
 import Data.List
 
+import DB
+
 main = do
  content <- parseCSVFromFile "titanic.csv"
  let Right tabl = content
@@ -9,4 +11,15 @@ main = do
  let properties = map (\v -> zip headers v) values
  let transform ((_,s):ps) = map (\(p,v) -> (s,p,v)) ps
  let graph      = concat (map transform properties)
- print (graph!!100)
+ let grph       = filter (\(_,_,x) -> x/="") graph
+ -- print (length grph) -- 8935
+ writeFile "titanic.dhs" (show grph)
+ let n = div (length grph) 2
+ writeFile "titanic1.dhs" (show (take n grph))
+ writeFile "titanic2.dhs" (show (drop n grph))
+
+sample1 = do
+ file <- readFile "titanic1.dhs"
+ let db = read file :: Graph
+ let r = answer [("1","X","Y")] db []
+ print (r)
